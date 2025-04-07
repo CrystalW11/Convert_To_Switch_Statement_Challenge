384e5a409c1473d948eba25dfe38d601bcec4463
CrystalW11 <crystal.warmack@gmail.com>
2025-04-07 16:38:00 -0600

    updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Convert_To_Switch_Statement_Challenge
+
+![alt text](image.png)
--- obj/Debug/net9.0/19-Convert_To_Switch_Statement_Challenge.AssemblyInfo.cs
+++ obj/Debug/net9.0/19-Convert_To_Switch_Statement_Challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("19-Convert_To_Switch_Statement_Challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c71b3a75dc954d2f2eb2d1ccaa96e96a1b6369")]
 [assembly: System.Reflection.AssemblyProductAttribute("19-Convert_To_Switch_Statement_Challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("19-Convert_To_Switch_Statement_Challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
